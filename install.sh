44d74453887dbfdb08c5f021d0612c87777f0dc5
Pranjal Aggarwal <pranjal2041@gmail.com>
2022-08-26 23:50:23 +0530

    Sub Ready

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 git clone https://github.com/Pranjal2041/COL761-MineBusters.git
 cd COL761-MineBusters
+unzip HW1_2019CS50443.zip
 module load compiler/python/3.6.0/ucs4/gnu/447
 module load compiler/gcc/7.1.0/compilervars
 module load pythonpackages/3.6.0/matplotlib/3.0.2/gnu
+cd HW1_2019CS50443
